f45de88d0f81dc4ab97e844e35d54abf7cdff8f9
Lauro235 <lorentzbloomguitar@gmail.com>
2025-11-04 19:55:40 +0000

    add more styling and some template html

--- format.js
+++ format.js
@@ -1,3 +1,28 @@
-const windowY = window.innerHeight;
+// classes
 
-console.log(windowY);
+class ContainerDimensions {
+  constructor(height,width) {
+    this.height = height;
+    this.width = width;
+  }
+}
+
+// GLOBAL VARIABLES
+const title = document.getElementById("title");
+const commandInput = document.getElementById("command__input")
+const nodeContainer = document.getElementById("node__container");
+let containerDimensions = new ContainerDimensions(window.innerHeight, window.innerWidth)
+
+// place node container beneath title.
+nodeContainer.style.top = title.clientHeight + 32 + "px"
+
+// Update node window height
+nodeContainer.style.height = (containerDimensions.height - (title.clientHeight + commandInput.offsetHeight + 64)) + "px"
+
+window.addEventListener('resize', function() {
+    containerDimensions.width = window.innerWidth;
+    containerDimensions.height = window.innerHeight;
+    // Update the DOM or perform other actions
+    console.log(containerDimensions);
+    
+});
